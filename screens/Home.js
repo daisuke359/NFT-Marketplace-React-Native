e5e483d65d0bfb0f31a1bd68f413169a777ba168
Daisuke <dk.joker.1107@icloud.com[credential]>
2022-05-30 14:34:35 -0700

    add header

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import { Text } from 'react-native';
+import {React, useState} from 'react';
+import { SafeAreaView, View, FlatList, Text } from 'react-native';
+
+import {COLORS, NFTData} from "../constants";
+import { HomeHeader, NFTCard, FocusedStatusBar } from '../comonents';
 
 const Home = () => {
   return (
-    <Text>Home</Text>
-  )
+    <SafeAreaView style={{flex: 1}}>
+      <FocusedStatusBar background={COLORS.primary} />
+      <View style={{flex:1}}>
+        <View style={{zIndex: 0}}>
+          <FlatList 
+            data={NFTData}
+            renderItem={({item}) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsHorizontalScrollIndicator={false}
+            ListHeaderComponent={HomeHeader}
+          />
+        </View>
+        <View style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          zIndex: -1
+        }}>
+          <View style={{ height: 300, backgroundColor: COLORS.primary }}/>
+          <View style={{ flex: 1, backgroundColor: COLORS.white }}/>
+        </View>
+      </View>
+
+    </SafeAreaView>
+  
+  );
 }
 
 export default Home
